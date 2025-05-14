732128181b12267cab1ca5b42855d73ad5278782
Yaroslav <solomatin.yarik@mail.ru>
2025-05-14 22:22:21 +0300

    updated gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
 const { program } = require('commander');
 
 program
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.1.0', '-V, --version', 'output the version number');
+    .version('0.1.0', '-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format');
 
 program.parse(process.argv);

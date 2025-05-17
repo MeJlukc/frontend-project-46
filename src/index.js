7339c22bf25d482d3881965aa55d11e84fc6d454
Yaroslav <solomatin.yarik@mail.ru>
2025-05-18 00:30:53 +0300

    modified bin/ and added src/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+import * as fs from 'node:fs';
+import * as yaml from 'js-yaml';
 
+const getDataType = (data) => data.split('.').at(-1);
 
-const genDiff = () => {
+const readData = (data) => fs.readFileSync(data);
+
+const parseData = (data, type) => {
+    switch (type) {
+        case 'json':
+            return JSON.parse(data);
+        case 'yaml' | 'yml':
+            return yaml.load(data);
+        default:
+            throw Error(`Unsupported data type: ${type}`);
+
+    }
+}
+
+const genDiff = (filepath1, filepath2) => {
+    const data1 = readData(filepath1);
+    const data2 = readData(filepath2);
+    const dataType1 = getDataType(filepath1);
+    const dataType2 = getDataType(filepath2);
 
 }
 

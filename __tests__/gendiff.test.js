c7ab0ae4036fff7ac8fe5ea294b16eb94d49b79f
Yaroslav <solomatin.yarik@mail.ru>
2025-05-25 16:44:03 +0300

    updated tests and added .yml support

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,8 +3,13 @@ import { expect, test } from '@jest/globals';
 import parse from '../src/parse.js';
 import { readData } from '../src/utilities.js';
 
-test('Compare files', () => {
+test('Compare .json files', () => {
     const result = genDiff('__fixtures__/testFile1.json', '__fixtures__/testFile2.json');
-    const expected = parse(readData('__fixtures__/testFileResult.json'), 'json');
+    const expected = parse(readData('__fixtures__/testFileResult.json'), '.json');
     expect(result).toEqual(expected);
+});
+
+test('Compare .yml files', () => {
+    const result = genDiff('__fixtures__/testFile1.yml', '__fixtures__/testFile2.yml');
+    const expected = parse(readData('__fixtures__/testFileResult.yml'), '.yml')
 });

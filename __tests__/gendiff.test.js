a3caab7a6d4a2aa4629e88b1e82be919650d7214
Yaroslav <solomatin.yarik@mail.ru>
2025-05-25 19:14:31 +0300

    updated tests and fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,15 +1,16 @@
 import genDiff from '../src/index.js';
 import { expect, test } from '@jest/globals';
-import parse from '../src/parse.js';
 import { readData } from '../src/utilities.js';
 
+const normalize = (str) => str.trim().replace(/\r/g, '')
 test('Compare .json files', () => {
     const result = genDiff('__fixtures__/testFile1.json', '__fixtures__/testFile2.json');
-    const expected = parse(readData('__fixtures__/testFileResult.json'), '.json');
-    expect(result).toEqual(expected);
+    const expected = readData('__fixtures__/testFileResult.txt');
+    expect(normalize(result)).toEqual(normalize(expected));
 });
 
 test('Compare .yml files', () => {
     const result = genDiff('__fixtures__/testFile1.yml', '__fixtures__/testFile2.yml');
-    const expected = parse(readData('__fixtures__/testFileResult.yml'), '.yml')
+    const expected = readData('__fixtures__/testFileResult.txt');
+    expect(normalize(result)).toEqual(normalize(expected));
 });

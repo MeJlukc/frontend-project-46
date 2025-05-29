4396a670563ad8579017d658ea92c118681f325e
Yaroslav <solomatin.yarik@mail.ru>
2025-05-29 17:59:30 +0300

    added format plain

---
+++ src/format/plain.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+const formatValue = (value) => {
+  if (_.isPlainObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return String(value);
+};
+
+const plain = (tree, parent = '') => {
+  const lines = tree
+    .filter((node) => node.type !== 'unchanged')
+    .flatMap((node) => {
+      const property = parent ? `${parent}.${node.key}` : node.key;
+
+      switch (node.type) {
+        case 'added':
+          return `Property '${property}' was added with value: ${formatValue(node.value)}`;
+        case 'removed':
+          return `Property '${property}' was removed`;
+        case 'changed':
+          return `Property '${property}' was updated. From ${formatValue(node.value1)} to ${formatValue(node.value2)}`;
+        case 'nested':
+          return plain(node.children, property);
+        default:
+          throw new Error(`Unknown type: ${node.type}`);
+      }
+    });
+
+  return lines.join('\n');
+};
+
+export default plain;

19c9f20c64c3e9ec26082253a49f53b5d6a6a970
Yaroslav <solomatin.yarik@mail.ru>
2025-06-02 17:35:11 +0300

    linter fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,20 +6,20 @@ const normalize = (str) => str.trim().replace(/\r/g, '')
 test('Compare .json files', () => {
     const result = genDiff('__fixtures__/testFile1.json', '__fixtures__/testFile2.json');
     const expected = readData('__fixtures__/testFileResult.txt');
-    expect(normalize(result)).toEqual(normalize(expected));
-});
+expect(normalize(result)).toEqual(normalize(expected));
+})
 
 test('Compare .yml files', () => {
     const result = genDiff('__fixtures__/testFile1.yml', '__fixtures__/testFile2.yml');
     const expected = readData('__fixtures__/testFileResult.txt');
     expect(normalize(result)).toEqual(normalize(expected));
-});
+})
 
 test('Plain formatting', () => {
     const result = genDiff('__fixtures__/testFile1.json', '__fixtures__/testFile2.json', 'plain');
     const expected = readData('__fixtures__/testFileResultPlain.txt');
     expect(normalize(result)).toEqual(normalize(expected));
-});
+})
 
 test('JSON formatting', () => {
     const result = genDiff('__fixtures__/testFile1.json', '__fixtures__/testFile2.json', 'json');
